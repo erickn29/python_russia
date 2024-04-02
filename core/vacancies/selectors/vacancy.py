629b5a3a2941b773f9b9bc824276321b49246f46
ya.novikov <arhdomain@gmail.com>
2024-04-02 21:40:03 +0300

    style and salary filter

--- core/vacancies/selectors/vacancy.py
+++ core/vacancies/selectors/vacancy.py
@@ -1,7 +1,8 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Q
 
 import django_filters
-from django.forms import BooleanField
+from django.forms import BooleanField, FloatField
+from django.http import QueryDict
 
 from vacancies.models import Vacancy
 
@@ -13,17 +14,69 @@ class VacancyFilter(django_filters.FilterSet):
             "created_at",
             "title",
             "text",
+            "grade",
             "stack",
             "link",
             "salary_to",
-            "company",
         )
+
+    company = django_filters.CharFilter(
+        field_name="company__name",
+        lookup_expr="icontains",
+        label="Компания",
+    )
+    salary_from = django_filters.NumberFilter(
+        # field_name="salary_from",
+        method="salary_filter",
+        label="Зарплата от",
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for _, field in self.form.fields.items():
+        for name, field in self.form.fields.items():
             field.widget.attrs["class"] = "form-control mb-4"
+            if name == "salary_from":
+                field.widget.attrs["value"] = 30000
+
+    def salary_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(
+                Q(salary_from__gte=int(value)) |
+                Q(salary_from=None, salary_to__gte=value))
+        return queryset
 
 
 def vacancy_queryset(filters) -> QuerySet[Vacancy]:
     return VacancyFilter(filters).qs
+
+
+# class StudentFilterSet(rest_filters.FilterSet):
+#     fullname = rest_filters.CharFilter(
+#         method="fullname_filter", label="active_contract"
+#     )
+#     school_came_from_is_empty = rest_filters.BooleanFilter(
+#         field_name="school_came_from", lookup_expr="isnull"
+#     )
+#     school_going_to_is_empty = rest_filters.BooleanFilter(
+#         field_name="school_going_to", lookup_expr="isnull"
+#     )
+#
+#     def fullname_filter(self, queryset, name, value):
+#         if value:
+#             return queryset.filter(search_fullname(fullname=value))
+#         return queryset
+#
+#     class Meta:
+#         model = Student
+#         fields = [
+#             "first_name",
+#             "last_name",
+#             "fullname",
+#             "patronymic",
+#             "grade",
+#             "education_cost",
+#             "school",
+#             "school_came_from_is_empty",
+#             "school_going_to_is_empty",
+#             "lms_id",
+#         ]
